9e23712fdab6b1ad025991379a9e8d2eb96e9d13
MEETPATEL322 <meet1patel0@gmail.com>
2025-08-14 21:56:09 +0530

    demo 2

--- server/src/index.ts
+++ server/src/index.ts
@@ -9,6 +9,8 @@ const PORT = config.port;
 connectDB();
 console.log("demo")
 
+console.log("Demo again")
+
 app.use(express.json({ limit: "100mb" }));
 app.use(express.urlencoded({ limit: "100mb", extended: true }));
 
